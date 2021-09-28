f39628c4a568db7b0bbd4da07377917c789bc6a9
Manuel Fernandez Glez <56442983+Unam29@users.noreply.github.com>
2021-09-28 20:57:31 +0200

    Create Mayor2.js

---
+++ Seminarios/Mayor2.js
@@ -0,0 +1,11 @@
+const r = Math.floor(Math.random()*10)+1;
+if(r>2){
+    console.log('es mayor que 2');
+    }
+    else if(r<2){
+        console.log('es menor que 2');
+    }
+    else if(r=2){
+        console.log('es igual a dos');
+    }
+
